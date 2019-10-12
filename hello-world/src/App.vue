4ac1e83206b3a5ddb52925d725f41483a3209050
brianhotopp <brihoto@gmail.com>
2019-10-12 15:40:13 -0400

    added my own component

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -2,16 +2,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld></HelloWorld>
+    <doorman></doorman>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import doorman from './components/doorman.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    doorman
   }
 }
 </script>
---
+++ hello-world/src/components/doorman.vue
@@ -0,0 +1,8 @@
+<template>
+    <p>kappa123</p>
+</template>
+<script>
+export default {
+    name : "doorman"
+}
+</script>

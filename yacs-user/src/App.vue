d5a84c28ac398f39552a894862d1178b9a851ad6
brianhotopp <brihoto@gmail.com>
2019-10-21 18:31:45 -0400

    I just realized I have to use bootstrap-vue instead of just bootstrap. Case in point this commit.

--- yacs-user/src/App.vue
+++ yacs-user/src/App.vue
@@ -8,15 +8,31 @@
       <nav class="navbar navbar-custom navbar-expand-sm flex-wrap2 flex-md-nowrap p-0">
         <div class="container">
           <a class="navbar-brand col-auto mr-0" href="https://yacs.cs.rpi.edu/"><img class="theme-logo" src="./assets/yacs-symbol-lockup-white.png"></a>
-          
         </div>
-        
-            
-        
       </nav>
     </div>
     <!-- Sidebar -->
-    <router-view></router-view>
+    <div class="container ml-0">
+      <div class = "row">
+          <div class="col-md-2" id="sidebar">
+              <ul class="nav flex-column">
+                  <li class="nav-item">
+                      <router-link class="nav-link white-link" to="#">Login</router-link>
+                  </li>
+                  <li class="nav-item">
+                      <router-link class="nav-link white-link" to="#">Browsing History</router-link>
+                  </li>
+                  <li class="nav-item">
+                      <router-link class="nav-link white-link" to="#">Notifications</router-link>
+                  </li>
+                  <li class="nav-item">
+                      <router-link class="nav-link white-link" to="#">Watch List</router-link>
+                  </li>
+              </ul>
+          </div>
+          <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,10 +52,20 @@ export default {
   color: #2c3e50;
   
 }
+.white-link{
+    color:#ffffff
+}
+.black-link{
+    color:black;
+}
 .theme-logo{
     height: 39px;
 }
 .navbar-custom{
   background-color: #c65353;
+}
+#sidebar{
+    background-color: #c65353;
+    color: #ffffff;
 }
 </style>
--- yacs-user/src/components/login.vue
+++ yacs-user/src/components/login.vue
@@ -1,45 +1,34 @@
 <template>
-<!-- new -->
     <div>
-    <div class="login-form">
-    <form class="formstyle">
-        <h2 class="text-center">Sign in</h2>       
-        <div class="form-group">
-            <label for="loginemail">Email address</label>
-            <input class="form-control" id="loginemail" placeholder="name@example.com" v-model="form.email">
+        <div class="col mt-5">
+            <div class="login-form"  style="max-width: 400px;">
+                <form class="mb-4 p-4">
+                    <h2 class="text-center">Sign in</h2>       
+                    <div class="form-group">
+                        <label for="loginemail">Email address</label>
+                        <input class="form-control" id="loginemail" placeholder="name@example.com" v-model="form.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="loginpass">Password</label>
+                        <input class="form-control" id="loginpass" placeholder="password" v-model="form.password">
+                    </div>
+                    <div class="form-group">
+                        <button type="button" class="btn btn-outline-light" v-on:click="onSubmit()">Sign-in</button>
+                    </div>
+                        <a href="#" class="pull-right white-link">Forgot Password?</a>
+                </form>
+                <p class="text-center"><router-link class="black-link" to="/register">Create an Account</router-link></p> 
+            </div>
         </div>
-        <div class="form-group">
-            <label for="loginpass">Password</label>
-            <input class="form-control" id="loginpass" placeholder="password" v-model="form.password">
-        </div>
-        <div class="form-group">
-            <button type="button" class="btn btn-outline-light" v-on:click="onSubmit()">Sign-in</button>
-        </div>
-        <div class="clearfix">
-            <a href="#" class="pull-right white-link">Forgot Password?</a>
-        </div>        
-    </form>
-    <p class="text-center"><a href="/register" class="black-link">Create an Account</a></p>
-</div>
-</div>
-  
-  
+    </div>
 </template>
 <style scoped>
-.formstyle{
+
+
+.login-form form {
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
     color: #ffffff;
     background-color: #c65353;
-}
-.login-form {
-    width: 340px;
-    margin: 50px auto;
-    
-}
-.login-form form {
-    margin-bottom: 15px;
-    
-    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-    padding: 30px;
 }
 .login-form h2 {
     margin: 0 0 15px;
@@ -49,12 +38,7 @@
     border-radius: 2px;
     
 }
-.white-link{
-    color:#ffffff
-}
-.black-link{
-    color:black;
-}
+
 </style>
 <script>
 import axios from 'axios'

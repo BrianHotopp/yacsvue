392ba105aacdc2bb64310898bc9910de49ad474e
brianhotopp <brihoto@gmail.com>
2019-10-21 14:49:06 -0400

    restyled the login page and added global navbar with logo

--- yacs-user/src/App.vue
+++ yacs-user/src/App.vue
@@ -7,9 +7,12 @@
     <div class="header-container">
       <nav class="navbar navbar-custom navbar-expand-sm flex-wrap2 flex-md-nowrap p-0">
         <div class="container">
-
+          <a class="navbar-brand col-auto mr-0" href="https://yacs.cs.rpi.edu/"><img class="theme-logo" src="./assets/yacs-symbol-lockup-white.png"></a>
+          
         </div>
-      
+        
+            
+        
       </nav>
     </div>
     <!-- Sidebar -->
@@ -26,11 +29,17 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-
+  
+}
+.theme-logo{
+    height: 39px;
+}
+.navbar-custom{
+  background-color: #c65353;
 }
 </style>
--- yacs-user/src/components/login.vue
+++ yacs-user/src/components/login.vue
@@ -1,22 +1,61 @@
 <template>
-    <div class="container">
+<!-- new -->
+    <div>
+    <div class="login-form">
+    <form class="formstyle">
+        <h2 class="text-center">Sign in</h2>       
         <div class="form-group">
-            <div class="row justify-content-center justify-content-center">
-                <div class="col-sm-4">
-                    <label for="loginemail">Email address</label>
-                    <input type="email" class="form-control" id="loginemail" placeholder="name@example.com" v-model="form.email">
-                </div>
-            </div>
-        <div class="row justify-content-center justify-content-center">
-            <div class="col-sm-4">
-                <label for="loginpass">Password</label>
-                <input type="password" class="form-control" id="loginpass" placeholder="password" v-model="form.password">
-            </div>
+            <label for="loginemail">Email address</label>
+            <input class="form-control" id="loginemail" placeholder="name@example.com" v-model="form.email">
         </div>
-    </div>
-    <b-button class = "mt-3" type="submit" variant="primary" v-on:click="onSubmit()">Submit</b-button>
-  </div>
+        <div class="form-group">
+            <label for="loginpass">Password</label>
+            <input class="form-control" id="loginpass" placeholder="password" v-model="form.password">
+        </div>
+        <div class="form-group">
+            <button type="button" class="btn btn-outline-light" v-on:click="onSubmit()">Sign-in</button>
+        </div>
+        <div class="clearfix">
+            <a href="#" class="pull-right white-link">Forgot Password?</a>
+        </div>        
+    </form>
+    <p class="text-center"><a href="/register" class="black-link">Create an Account</a></p>
+</div>
+</div>
+  
+  
 </template>
+<style scoped>
+.formstyle{
+    color: #ffffff;
+    background-color: #c65353;
+}
+.login-form {
+    width: 340px;
+    margin: 50px auto;
+    
+}
+.login-form form {
+    margin-bottom: 15px;
+    
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    padding: 30px;
+}
+.login-form h2 {
+    margin: 0 0 15px;
+}
+.form-control{
+    min-height: 38px;
+    border-radius: 2px;
+    
+}
+.white-link{
+    color:#ffffff
+}
+.black-link{
+    color:black;
+}
+</style>
 <script>
 import axios from 'axios'
 export default {

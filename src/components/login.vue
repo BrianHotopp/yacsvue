3b8bf8cd4cac7ca46466cc9b28246f224aa8bd98
brianhotopp <brihoto@gmail.com>
2019-10-22 18:57:59 -0400

    changed buttons on login page

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,36 +1,26 @@
 <template>
     <div class="w-100" style="max-width: 500px">
-        <form class="mb-5 p-5 mt-5">
-            <h2 class="text-center">Sign in</h2>       
+        <form class="p-5 mt-5">
+            <h2 class="float-left">Sign in</h2><br>       
             <div class="form-group">
-                <label for="loginemail">Email address</label>
                 <input class="form-control" id="loginemail" placeholder="email" v-model="form.email">
             </div>
             <div class="form-group">
-                <label for="loginpass">Password</label>
                 <input class="form-control" id="loginpass" placeholder="password" v-model="form.password">
             </div>
             <div class="form-group">
-                <button type="button" class="btn btn-outline-light" v-on:click="onSubmit()">Sign-in</button>
+                <button type="button" class="btn btn-primary float-left" v-on:click="onSubmit()">Sign-in</button>
+                <button type="button" to="/register" class="btn btn-success float-right"><router-link tag="div" to="/register">Create an Account</router-link></button>
+                <button type="button" to="#" class="btn btn-danger"><router-link tag="div" to="/register">Forgot Password</router-link></button>
             </div>
-                <a href="#" class="pull-right white-link">Forgot Password?</a>
         </form>
         
-        <p class="text-center">
-            <button type="button" class="btn btn-outline-dark"><router-link to="/register">Create an Account</router-link></button>
-        </p> 
+        
+            
     </div>
 </template>
 <style scoped>
-.btn a{  
-    text-decoration: none;
-    color: #324455;
 
-}
-.btn a:hover{  
-    color:white;
-
-}
 
 </style>
 <script>
